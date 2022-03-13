08224ebbce009705cc44da33c81830045394117e
kiergit <60729046+kiergit@users.noreply.github.com>
2022-03-13 00:46:25 -0800

    added website files

---
+++ app/controllers/Application.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import play.mvc.Controller;
+
+public class Application extends Controller {
+
+    public static void home() {
+        render();
+    }
+    public static void skills() {render();}
+
+
+}
